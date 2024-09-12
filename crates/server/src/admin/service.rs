24d931c46028a1481e28324657e413c8cd2e8420
alenmestrov <alenmestrov@gmail.com>
2024-09-12 10:49:53 +0200

    testing workflows2

--- crates/server/src/admin/service.rs
+++ crates/server/src/admin/service.rs
@@ -64,7 +64,7 @@ pub struct AdminState {
     pub ctx_manager: ContextManager,
 }
 
-// Embed the contents of the admin-ui build directory into the binary
+// Embed the admin-ui build directory into the binary
 static REACT_STATIC_FILES: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../../node-ui/build");
 
 pub(crate) fn setup(
